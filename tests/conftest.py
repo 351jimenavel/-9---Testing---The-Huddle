254e62ffd0c7903862970ef6e745008a76b73b3e
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-10-14 14:45:15 -0300

    feat: completa funcionalidad de puerto libre

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,10 @@ def puerto_libre():
     Hace: reserva un puerto efimero y lo devuelve.
     Uso: evita choques entre tests.
     '''
-    pass
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("", 0))                          # el sistema operativo asignará uno automáticamente
+    puerto_asignado = sock.getsockname()[1]     # get sock name devuelve la direccion IP y el puerto por eso puse [1] para que sea solo el puerto
+    return puerto_asignado
 
 @pytest.fixture
 def server_en_marcha(puerto):
