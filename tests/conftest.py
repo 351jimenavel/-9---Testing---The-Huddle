ccb39b2836273c65ec7b8bef8d10b619eaa1bce3
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-10-16 13:15:24 -0300

    feat: configuracion de path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,4 @@
-import pytest
-import socket
-
-@pytest.fixture
-def puerto_libre():
-    '''
-    Hace: reserva un puerto efimero y lo devuelve.
-    Uso: evita choques entre tests.
-    '''
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("", 0))                          # el sistema operativo asignará uno automáticamente
-    puerto_asignado = sock.getsockname()[1]     # get sock name devuelve la direccion IP y el puerto por eso puse [1] para que sea solo el puerto
-    return puerto_asignado
-
-@pytest.fixture
-def server_en_marcha(puerto):
-    '''
-    Hace: levanta el server en hilo/proceso; espera hasta que este listo.
-    Teardown: cierra ordenado y verifica que el puerto quede libre
-    '''
-    pass
-
-@pytest.fixture
-def cliente_factory(puerto):
-    '''
-    Hace: Enviar(texto), recibir(timeout), cerrar(), cerrar_abrupto()
-    '''
-    pass
+import os, sys
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
